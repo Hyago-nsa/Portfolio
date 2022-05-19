55b1b7e940d1e4dc550c21199c6bc9324fcd97a1
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-05-19 19:14:27 -0300

    Add project

---
+++ src/components/services/data_services.js
@@ -0,0 +1,60 @@
+import IMG1 from "../../assets/icon-1.png"
+import IMG2 from "../../assets/icon-2.png"
+import IMG3 from "../../assets/icon-3.png"
+import IMG4 from "../../assets/icon-4.png"
+import IMG5 from "../../assets/icon-5.png"
+import IMG6 from "../../assets/icon-6.png"
+
+
+const data = [
+    {
+        id: 1,
+        img: IMG1,
+        alt: "HTML 5",
+        title: "HTML 5",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        toGithub: "Link"
+    },
+    {
+        id: 2,
+        img: IMG2,
+        alt: "CSS 3",
+        title: "CSS 3",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        toGithub: "Link"
+    },
+    {
+        id: 3,
+        img: IMG3,
+        alt: "JavaScript",
+        title: "JavaScript",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        toGithub: "Link"
+    },
+    {
+        id: 4,
+        img: IMG4,
+        alt: "TypeScript",
+        title: "TypeScript",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        toGithub: "Link"
+    },
+    {
+        id: 5,
+        img: IMG5,
+        alt: "Angular",
+        title: "Angular",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        toGithub: "Link"
+    },
+    {
+        id: 6,
+        img: IMG6,
+        alt: "React",
+        title: "React",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        toGithub: "Link"
+    }
+]
+
+export default data;
